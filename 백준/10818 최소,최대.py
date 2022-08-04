1d08b899d4e8c09ac8680c151fdeaceda324d5c1
moooonam <anska77@naver.com>
2022-08-04 17:32:51 +0900

    up

---
+++ 백준/10818 최소,최대.py
@@ -0,0 +1,6 @@
+#10818 최소,최대
+N=int(input())
+my_list=list(map(int,input().split()))
+a =max(my_list)
+b =min(my_list)
+print(b, a)
---
+++ 백준/2742 기찍N.py
@@ -0,0 +1,4 @@
+#2742 기찍N
+N =int(input())
+for i in range(N ,0,-1):
+    print(i)
