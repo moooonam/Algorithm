fdc24f77e6abf6e43738a71b06c8a01dc8e14df8
moooonam <anska77@naver.com>
2022-08-18 01:05:23 +0900

    up

--- 백준/10_브루트 포스/영화감독 숌.py
+++ 백준/10_브루트 포스/영화감독 숌.py
@@ -2,14 +2,17 @@
 N = int(input())
 count = 1
 num = 1600
-while N != 666:
-    num += 1
-    num_list = list(map(int, str(num)))
-    for i in range(len(num_list)-2):
-        if num_list[i]==6 and num_list[i+1]==6 and num_list[i+2]==6:
-            count+=1
+if N==1:
+    print(666)
+else:
+    while N != 666:
+        num += 1
+        num_list = list(map(int, str(num)))
+        for i in range(len(num_list)-2):
+            if num_list[i]==6 and num_list[i+1]==6 and num_list[i+2]==6:
+                count+=1
+                break
+        if count == N:
             break
-    if count == N:
-        break
-print(num)    
+    print(num)    
 
