1607444769d87923865f1f69d913dcf6697e19f3
moooonam <anska77@naver.com>
2023-06-26 10:17:06 +0900

    230626 algo up

--- 백준/기타/2579 계단오르기.py
+++ 백준/기타/2579 계단오르기.py
@@ -1,5 +1,16 @@
 N = int(input())
-arr =[0]
+arr =[]
+dp = []
 for _ in range(N):
     arr.append(int(input()))
 
+if len(arr)<=2: 
+    print(sum(arr))
+else:
+    dp.append(arr[0])
+    dp.append(max(arr[0]+arr[1],arr[1]))
+    dp.append(max(arr[0]+arr[2],arr[1]+arr[2]))
+    for i in range(3,N):
+        dp.append(max(dp[i-2] + arr[i] , dp[i-3] + arr[i] + arr[i - 1]))
+
+    print(dp.pop())
